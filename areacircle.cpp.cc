9d2139ac3f7ffb9f2a82cca4d8acb1e79acefa49
vaibhaviranmale3647-ui <vaibhaviranmale3647@gmail.com>
2026-07-15 10:57:09 +0530

    Add files via upload

---
+++ areacircle.cpp.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,pi;
+  cout<<"Enter two numbers: ";
+  cin>>a>>pi;
+  cout<<"area= "<<pi*a*a;
+}
---
+++ para.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b;
+  cout<<"Enter two numbers: ";
+  cin>>a>>b;
+  cout<<"para= "<<2(a*b);
+}
